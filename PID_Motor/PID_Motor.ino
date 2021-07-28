52191ae173399ad315c417aab2cd3ab26bc51059
Haadi Khan <haadikhan05@gmail.com>
2021-07-28 00:32:47 -0400

    Added Diagrams and Cleaned Code
    First of all, I added the CAD File as well as the circuit diagram.
    Secondly, I edited the .ino to be more readable and hopefully
    easier to edit and tweak if need be.

--- PID_Motor/PID_Motor.ino
+++ PID_Motor/PID_Motor.ino
@@ -5,14 +5,16 @@
 #define ENCB 3 // white wire
 
 // Motor
-#define PWM 4
-#define IN1 6
-#define IN2 8
+#define PWM 5
+#define IN1 4
+#define IN2 6
 
+// Defining the target as a global variable is only useful if the target has a set location. If the target is variable, say following a trig function, define it in the loop() function.
+int target = 90;
 volatile int pos = 0;
 long prevT = 0;
 float prevErr = 0;
-float eintegral = 0;
+float absement = 0;
 
 void setup() {
     Serial.begin(9600);
@@ -22,56 +24,35 @@ void setup() {
     pinMode(PWM, OUTPUT);
     pinMode(IN1, OUTPUT);
     pinMode(IN2, OUTPUT);
-    Serial.println("target currPos");
+    Serial.println("u");
 }
 
 void loop() {
-    // set target position
-    //int target = 1200;
-    int target = 2100;
-
-    // PID constants
-    float kp = 1;
-    float ki = 0;
-    float kd = 0;
-
-    // time difference
-    long currT = micros();
-    float deltaT = ((float)(currT - prevT)) / (1.0e6);
-    prevT = currT;
-
-    // Read the position in an atomic block to avoid a potential
-    // misread if the interrupt coincides with this code running
     int currPos = 0;
     ATOMIC_BLOCK(ATOMIC_RESTORESTATE) {
         currPos = pos;
     }
 
-    // position, absement, velocity
+    // first three parameters are kP, kI, and kD
+    float u = pidControl(1, 0, 0, currPos);
+    float pwr = fabs(u) > 255 ? 255 : fabs(u); // fabs() is float absolute value
+    int dir = u < 0 ? -1 : 1;
+    setMotor(dir, pwr, PWM, IN1, IN2);
+    Serial.print(u);
+    Serial.println();
+}
+
+float pidControl(float kP, float kI, float kD, int currPos) {
+    long currT = micros();
+    float deltaT = ((float)(currT - prevT)) / (1.0e6);
+    prevT = currT;
+
+    // err is our position, absement is our integral, and velocity is our derivative
     int err =  currPos - target;
     float absement = absement + err * deltaT;
     float velocity = (err - prevErr) / (deltaT);
-
-    // control signal
-    float u = kp * err + ki * absement +  kd * velocity;
-
-    // motor power
-    float pwr = fabs(u); // fabs() is float abs
-    pwr = pwr > 255 ? 255 : pwr;
-
-    // motor direction
-    int dir = u < 0 ? -1 : 1;
-
-    // signal the motor
-    setMotor(dir, pwr, PWM, IN1, IN2);
-
-    // store previous error
     prevErr = err;
-
-    Serial.print(target);
-    Serial.print(" ");
-    Serial.print(currPos);
-    Serial.println();
+    return kP * err + kI * absement +  kD * velocity;
 }
 
 void setMotor(int dir, int pwmVal, int pwm, int in1, int in2) {
@@ -94,4 +75,11 @@ void setMotor(int dir, int pwmVal, int pwm, int in1, int in2) {
 
 void readEncoder() {
     digitalRead(ENCB) ? pos++ : pos--;
+}
+
+void display(int target, int currPos) {
+    Serial.print(target);
+    Serial.print(" ");
+    Serial.print(currPos);
+    Serial.println();
 }
--- README.md
+++ README.md
@@ -6,12 +6,12 @@ PID Control, or Proportional Integral Derivative Control, is a control system us
 
 ## How to use
 
-This was created for a rather simple PID Motor demo utilizing a single motor and a few parts. All the parts combined (assuming you already have an arduino and some wires + basic stuff) should be under $20. The code should work no matter the robot, assuming of course it's a simple PID use case, but the CAD files are specialized for my use. If you would like to use this exact demo, you can buy [this motor](https://www.amazon.com/dp/B07GNGCNK1?psc=1&ref=ppx_yo2_dt_b_product_details) and any cheap motor driver chip.
+This was created for a simple PID Motor demo utilizing a single motor and a few parts. All the parts combined (assuming you already have an arduino and some wires + basic stuff) should be under $20. The code should work for most motors with an encoder, but the CAD file is specialized for my use. If you would like to use this exact demo, you can buy [this motor](https://www.amazon.com/dp/B07GNGCNK1?psc=1&ref=ppx_yo2_dt_b_product_details) and any cheap motor driver chip, althought I'd recommend [this](https://www.amazon.com/dp/B07WS89781?psc=1&ref=ppx_yo2_dt_b_product_details)
 
-To run the code just download the file and run it using the Arduino IDE. To simplify this for beginners, I'll attach a TinkerCAD diagram for the circuits: 
+To run the code just download the file and run it using the Arduino IDE. To simplify this for beginners, I'll attach a TinkerCAD diagram for the circuits:
 
-![Circuit Diagram]()
+![Circuit Diagram](Circuit_Diagram.png)
 
 <hr>
 
-I'm using this mostly to test out some auto PID tuning stuff I've been messing with so this is more just to mess around. 
+I'm using this mostly to test out some auto PID tuning stuff I've been playing with. Probably won't see a lot of updates and stuff.
