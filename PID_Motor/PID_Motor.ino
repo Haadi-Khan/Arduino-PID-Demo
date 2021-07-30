244e3822230e78e5a1b363c71cd8fd8d69e9ffd0
Haadi Khan <haadikhan05@gmail.com>
2021-07-29 20:09:32 -0400

    PID now "works"
    PID System now works for the motor, however it has some issues
    with adjusting and oscillating back after an overshoot. I'll have
    to figure out how to properly fix that.

--- PID_Motor/PID_Motor.ino
+++ PID_Motor/PID_Motor.ino
@@ -5,12 +5,12 @@
 #define ENCB 3 // white wire
 
 // Motor
-#define PWM 5
-#define IN1 4
-#define IN2 6
+#define IN1 13
+#define IN2 12
+#define PWM 11
 
 // Defining the target as a global variable is only useful if the target has a set location. If the target is variable, say following a trig function, define it in the loop() function.
-int target = 90;
+int target = 190;
 volatile int pos = 0;
 long prevT = 0;
 float prevErr = 0;
@@ -24,7 +24,6 @@ void setup() {
     pinMode(PWM, OUTPUT);
     pinMode(IN1, OUTPUT);
     pinMode(IN2, OUTPUT);
-    Serial.println("u");
 }
 
 void loop() {
@@ -34,52 +33,55 @@ void loop() {
     }
 
     // first three parameters are kP, kI, and kD
-    float u = pidControl(1, 0, 0, currPos);
-    float pwr = fabs(u) > 255 ? 255 : fabs(u); // fabs() is float absolute value
+    float u = pidControl(4.12, 2.1, 1, currPos);
+    float pwr = fabs(u) > 255 ? 255 : fabs(u); // This step causes issue
     int dir = u < 0 ? -1 : 1;
-    setMotor(dir, pwr, PWM, IN1, IN2);
-    Serial.print(u);
-    Serial.println();
+    setMotor(dir, pwr);
+    display(target, currPos);
 }
 
-float pidControl(float kP, float kI, float kD, int currPos) {
+float pidControl(float kP, float kI, float kD,int currPos) {
     long currT = micros();
     float deltaT = ((float)(currT - prevT)) / (1.0e6);
     prevT = currT;
 
     // err is our position, absement is our integral, and velocity is our derivative
-    int err =  currPos - target;
+    int err = target - currPos;
     float absement = absement + err * deltaT;
     float velocity = (err - prevErr) / (deltaT);
     prevErr = err;
     return kP * err + kI * absement +  kD * velocity;
 }
 
-void setMotor(int dir, int pwmVal, int pwm, int in1, int in2) {
-    analogWrite(pwm, pwmVal);
+void setMotor(int dir, int pwmVal) {
+    analogWrite(PWM, pwmVal);
     switch (dir) {
         case 1:
-            digitalWrite(in1, HIGH);
-            digitalWrite(in2, LOW);
+            digitalWrite(IN1, HIGH);
+            digitalWrite(IN2, LOW);
             break;
         case -1:
-            digitalWrite(in1, LOW);
-            digitalWrite(in2, HIGH);
+            digitalWrite(IN1, LOW);
+            digitalWrite(IN2, HIGH);
             break;
         default:
-            digitalWrite(in1, LOW);
-            digitalWrite(in2, LOW);
+            digitalWrite(IN1, LOW);
+            digitalWrite(IN2, LOW);
             break;
     }
 }
 
 void readEncoder() {
-    digitalRead(ENCB) ? pos++ : pos--;
+    digitalRead(ENCB) > 0 ? pos++ : pos--;
 }
 
 void display(int target, int currPos) {
+    Serial.print(0);
+    Serial.print(" ");
     Serial.print(target);
     Serial.print(" ");
     Serial.print(currPos);
+    Serial.print(" ");
+    Serial.print(target + 0.25*target); // these added numbers are to make the graph look better over time
     Serial.println();
 }
